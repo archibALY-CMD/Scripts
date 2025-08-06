c7c776e292d2d4efffc1743e27426d1a4282fb04
archibALY-CMD <artem.sheshukov2013@gmail.com>
2025-08-06 20:22:02 +0700

    Update OneProtocol.lua2
    С модам

--- Games/Arsenal/OneProtocol.lua
+++ Games/Arsenal/OneProtocol.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet('https://github.com/OneProtocol/Project/raw/main/Loader', true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AdvanceFTeam/Our-Scripts/main/AdvanceTech/Arsenal_V1.6.lua"))()
